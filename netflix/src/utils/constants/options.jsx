53d66b81a8a514ea62989fdf5554b1965d85bfe2
Nitish gautam <136136860+NitishGautam5@users.noreply.github.com>
2024-04-06 11:26:43 +0530

    Update options.jsx

--- netflix/src/utils/constants/options.jsx
+++ netflix/src/utils/constants/options.jsx
@@ -3,8 +3,8 @@ export const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTIxNTg3ZjYwMzIxNzRlNDhmN2M0ZjFmYTczYjBkZiIsInN1YiI6IjY1ZDA3NDI4N2I3YjRkMDE4NzU2ZTBiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.usLnsksADFIwyY23VR9ezOKJhKIF4qNdp0Lh2Wyt4QY'
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTIxNTg3ZjYwMzIxNJhKIF4qNdp0Lh2Wyt4QY'
     }
   };
 
-  export const openaikey = "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"
+  export const openaikey = "s"
